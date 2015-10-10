286c8e4c8effaf71b04bf2e634f7dfaae69fe3ec
nannemdp <david.nannemann@gmail.com>
2015-10-10 08:32:22 -0400

    code to generate plots

---
+++ plot3.R
@@ -0,0 +1,22 @@
+df <-read.table("household_power_consumption.txt", header=TRUE, sep=";")
+df[, "Global_active_power_kw"] <- as.numeric( df[, "Global_active_power"])/1000
+df[, "Date"] <- as.Date(df[, "Date"], format="%d/%m/%Y")
+small_df = df[df$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02") ) ,]
+
+small_df[,"ComboDate"] <- paste(small_df$Date, small_df$Time, sep=" ")
+small_df$ComboDatePosix <- strptime( small_df[,"ComboDate"], format="%Y-%m-%d %H:%M:%S")
+
+small_df[, "Sub_metering_1"] <- as.numeric( small_df[, "Sub_metering_1"])
+small_df[, "Sub_metering_2"] <- as.numeric( small_df[, "Sub_metering_2"])
+small_df[, "Sub_metering_3"] <- as.numeric( small_df[, "Sub_metering_3"])
+
+png("plot3.png")
+plot(small_df$ComboDatePosix , small_df$Sub_metering_1, type="l", 
+     ylab="Energy sub metering", xlab="")
+lines(small_df$ComboDatePosix , small_df$Sub_metering_2, 
+      ylab="", xlab="", col="red")
+points(small_df$ComboDatePosix , small_df$Sub_metering_3, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="", col="blue")
+legend("topright", lty=1,lwd=3, col=c("black","blue","red"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
+dev.off()
