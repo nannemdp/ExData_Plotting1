0dcc943dbdb4397062c3a671b146ea7d7dba525b
nannemdp <david.nannemann@gmail.com>
2015-10-10 09:49:54 -0400

    uncommented df read.table

--- plot3.R
+++ plot3.R
@@ -1,12 +1,12 @@
-#df <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("?"))
-#df <- df[complete.cases(df),]
+df <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("?"))
+df <- df[complete.cases(df),]
 
-#df[, "Date"] <- as.Date(df[, "Date"], format="%d/%m/%Y")
+df[, "Date"] <- as.Date(df[, "Date"], format="%d/%m/%Y")
 
-#small_df = df[df$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02") ) ,]
+small_df = df[df$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02") ) ,]
 
-#small_df[,"ComboDate"] <- paste(small_df$Date, small_df$Time, sep=" ")
-#small_df$ComboDatePosix <- strptime( small_df[,"ComboDate"], format="%Y-%m-%d %H:%M:%S")
+small_df[,"ComboDate"] <- paste(small_df$Date, small_df$Time, sep=" ")
+small_df$ComboDatePosix <- strptime( small_df[,"ComboDate"], format="%Y-%m-%d %H:%M:%S")
 
 png("plot3.png")
 plot(small_df$ComboDatePosix , small_df$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
--- plot4.R
+++ plot4.R
@@ -1,12 +1,12 @@
-#df <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("?"))
-#df <- df[complete.cases(df),]
+df <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("?"))
+df <- df[complete.cases(df),]
 
-#df[, "Date"] <- as.Date(df[, "Date"], format="%d/%m/%Y")
+df[, "Date"] <- as.Date(df[, "Date"], format="%d/%m/%Y")
 
-#small_df = df[df$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02") ) ,]
+small_df = df[df$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02") ) ,]
 
-#small_df[,"ComboDate"] <- paste(small_df$Date, small_df$Time, sep=" ")
-#small_df$ComboDatePosix <- strptime( small_df[,"ComboDate"], format="%Y-%m-%d %H:%M:%S")
+small_df[,"ComboDate"] <- paste(small_df$Date, small_df$Time, sep=" ")
+small_df$ComboDatePosix <- strptime( small_df[,"ComboDate"], format="%Y-%m-%d %H:%M:%S")
 
 png("plot4.png")
 par(mfrow=c(2,2))
